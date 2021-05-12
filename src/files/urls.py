fa5c645ae474f4d95bf37bbe349c307e9302ac89
ersain <ersain.subs@gmail.com>
2021-05-13 01:01:36 +0600

    Add download routes

--- src/files/urls.py
+++ src/files/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<str:code>/', views.FileRetrieveView.as_view()),
+    path('get/<str:code>/', views.ComicDownloadView.as_view()),
+    path('download/<str:code>/', views.ComicRedirectView.as_view()),
 ]
--- src/files/views.py
+++ src/files/views.py
@@ -1,9 +1,12 @@
+import requests
+from django.http import HttpResponse, FileResponse
 from rest_framework import status, permissions
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
-from .models import File
+from .models import File, FileChoices
 from .serializers import FileSerializer
+from .services.s3 import S3Services
 
 
 class FileRetrieveView(GenericAPIView):
@@ -19,3 +22,39 @@ class FileRetrieveView(GenericAPIView):
         obj: File = self.get_object()
         serializer = self.get_serializer(obj)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ComicDownloadView(GenericAPIView):
+    serializer_class = None
+    lookup_field = 'code'
+
+    def get_queryset(self):
+        return File.objects.filter(type=FileChoices.COMIC)
+
+    # noinspection PyMethodMayBeStatic
+    def get(self, request, *args, **kwargs):
+        obj: File = self.get_object()
+        object_url = S3Services.generate_object_url(obj.file.name)
+        r = requests.get(url=object_url, stream=True)
+        r.raise_for_status()
+        response = HttpResponse(r.raw, content_type='application/pdf')
+        response['Content-Disposition'] = f'inline; filename={obj.file.name}'
+        return response
+
+
+class ComicRedirectView(GenericAPIView):
+    serializer_class = None
+    lookup_field = 'code'
+
+    def get_queryset(self):
+        return File.objects.filter(type=FileChoices.COMIC)
+
+    # noinspection PyMethodMayBeStatic
+    def get(self, request, *args, **kwargs):
+        obj: File = self.get_object()
+        file_handle = obj.file.open()
+        response = FileResponse(file_handle, content_type='application/pdf')
+        response['Content-Length'] = obj.file.size
+        response['Content-Disposition'] = f'attachment; filename="{obj.file.name}"'
+
+        return response
