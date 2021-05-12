0abf0d17373d36050ee39fe99481fa08c48838be
ersain <ersain.subs@gmail.com>
2021-05-13 01:14:09 +0600

    Remove useless route

--- src/files/urls.py
+++ src/files/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<str:code>/', views.FileRetrieveView.as_view()),
-    path('get/<str:code>/', views.ComicDownloadView.as_view()),
-    path('download/<str:code>/', views.ComicRedirectView.as_view()),
+    path('download/<str:code>/', views.ComicDownloadView.as_view()),
 ]
--- src/files/views.py
+++ src/files/views.py
@@ -1,12 +1,10 @@
-import requests
-from django.http import HttpResponse, FileResponse
+from django.http import FileResponse
 from rest_framework import status, permissions
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from .models import File, FileChoices
 from .serializers import FileSerializer
-from .services.s3 import S3Services
 
 
 class FileRetrieveView(GenericAPIView):
@@ -25,24 +23,6 @@ class FileRetrieveView(GenericAPIView):
 
 
 class ComicDownloadView(GenericAPIView):
-    serializer_class = None
-    lookup_field = 'code'
-
-    def get_queryset(self):
-        return File.objects.filter(type=FileChoices.COMIC)
-
-    # noinspection PyMethodMayBeStatic
-    def get(self, request, *args, **kwargs):
-        obj: File = self.get_object()
-        object_url = S3Services.generate_object_url(obj.file.name)
-        r = requests.get(url=object_url, stream=True)
-        r.raise_for_status()
-        response = HttpResponse(r.raw, content_type='application/pdf')
-        response['Content-Disposition'] = f'inline; filename={obj.file.name}'
-        return response
-
-
-class ComicRedirectView(GenericAPIView):
     serializer_class = None
     lookup_field = 'code'
 
